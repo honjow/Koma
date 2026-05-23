7e842a82afcacd4beebeb24fe61bf6d6ceb8a6c7
honjow <honjow311@gmail.com>
2026-05-24 06:10:28 +0800

    chore: gitignore SDK target/ and worker artifacts

--- tools/wasm-runtime-spike/rust-sdk/target/debug/deps/koma_source_sdk-09768031f101f389.d
+++
@@ -1,5 +0,0 @@
-/home/gamer/git/Koma/tools/wasm-runtime-spike/rust-sdk/target/debug/deps/koma_source_sdk-09768031f101f389.d: src/lib.rs
-
-/home/gamer/git/Koma/tools/wasm-runtime-spike/rust-sdk/target/debug/deps/koma_source_sdk-09768031f101f389: src/lib.rs
-
-src/lib.rs:
--- tools/wasm-runtime-spike/rust-sdk/target/debug/deps/koma_source_sdk-448494a87b1e0ad7.d
+++
@@ -1,7 +0,0 @@
-/home/gamer/git/Koma/tools/wasm-runtime-spike/rust-sdk/target/debug/deps/koma_source_sdk-448494a87b1e0ad7.d: src/lib.rs
-
-/home/gamer/git/Koma/tools/wasm-runtime-spike/rust-sdk/target/debug/deps/libkoma_source_sdk-448494a87b1e0ad7.rlib: src/lib.rs
-
-/home/gamer/git/Koma/tools/wasm-runtime-spike/rust-sdk/target/debug/deps/libkoma_source_sdk-448494a87b1e0ad7.rmeta: src/lib.rs
-
-src/lib.rs:
